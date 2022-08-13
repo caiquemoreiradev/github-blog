872079effd6e38f11e04bb5605b7e4a36de93d9f
caique.moraes <caique.moraes@compasso.com.br>
2022-08-13 09:05:59 -0300

    feat: post card

---
+++ src/components/PostCard/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const PostCardContainer = styled.article`
+  background: ${(props) => props.theme['base-post']};
+  border-radius: 10px;
+  padding: 3.2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  > h2 {
+    color: ${(props) => props.theme['base-title']};
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 160%;
+    max-width: 25rem;
+  }
+
+  > span {
+    color: ${(props) => props.theme['base-span']};
+    font-weight: 400;
+    font-size: 1.4rem;
+  }
+`
+
+export const Content = styled.div`
+  > p {
+    font-weight: 400;
+    font-size: 1.6rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+`
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  padding: 0 2rem 5rem;
+`
+
+export const PostList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(39.6rem, auto));
+  gap: 3.2rem;
+  margin-top: 4.8rem;
+`
